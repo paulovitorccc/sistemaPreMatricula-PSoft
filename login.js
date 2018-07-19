cd0103b45d67cea9f3184c45499ecdca620853b0
pedrofwanderley <pedro.wanderley@ccc.ufcg.edu.br>
2018-07-18 23:22:47 -0300

    Login usando angular

---
+++ login.js
@@ -0,0 +1,4 @@
+function onLoadFunction(){
+    gapi.client.setApiKey('AIzaSyCZFWwAlFpyPlzq7ffKtC1LwpXvRmcG-0I');
+    gapi.client.load('plus', 'v1', function() {});
+}
---
+++ script.js
@@ -0,0 +1,35 @@
+var sysLogin = angular.module("sysLogin",[]);
+
+sysLogin.controller('loginController', ['$scope', function($scope) {
+    $scope.gmail = {
+        username: "",
+        email: ""
+    };
+
+    $scope.onGoogleLogin = function() {
+        var params = {
+            'clientid': '560484290696-j89t9e5apdb9ef2tcu2l7knoj0hhhbn5.apps.googleusercontent.com',
+            'cookiepolicy': 'single_host_origin',
+            'callback': function(result) {
+                if(result['status']['signed_in']) {
+                    var request = gapi.client.plus.people.get(
+                        {
+                            'userId': 'me'
+                        }
+                    );
+                    request.execute(function(resp){
+                        $scope.$apply(function(){
+                            $scope.gmail.username = resp.displayName;
+                             $scope.gmal.email = resp.emails[0].value;
+                        });
+                    });
+                }
+
+            },
+            'approvalprompt': 'force',
+            'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+         };
+
+         gapi.auth.signIn(params);
+    }
+}])
