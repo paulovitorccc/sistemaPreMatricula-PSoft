98554f3087f4687d75d7271ad42f5182584270ea
paulovitorccc <paulo.dantas@ccc.ufcg.edu.br>
2018-08-01 02:06:27 -0300

    Logica de redirecionamento para primeiro acesso ou nao

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller('myController', ['$scope', function($scope) {
+myApp.controller('myController', ['$scope', '$http', function($scope, $http) {
     $scope.gmail = {
         username: "",
         email: ""
     };
+    $scope.aluno = {}
 
     $scope.verifyEmailCCC = function(email) {
         const regExp = /@ccc.ufcg.edu.br/;
         return regExp.test(email);
-    }
+    };
+
+    $scope.verifyFirstAcess = function(email) {
+        var fullUsername = email.split("@");
+        var brokenUsername = fullUsername[0].split(".");
+        var getPath = "http://localhost:8080/prematricula/alunos/" +
+            brokenUsername[0] + "@" + brokenUsername[1];
+        $http.get(getPath)
+            .then(function (response) {
+                return response.data;
+            }, function (error) {
+                return null;
+            });
+    };
 
     $scope.verifyAcess = async function() {
         if($scope.verifyEmailCCC($scope.gmail.email)) {
-
-            window.location.assign("firstaccess.html");
+            var alunoRecovered = $scope.verifyFirstAcess($scope.gmail.email);
+            if(alunoRecovered === null) {
+                window.location.assign("firstaccess.html");
+            } else {
+                $scope.aluno = alunoRecovered;
+                console.log($scope.aluno);
+                window.location.assign("aluno.html");
+            }
         } else {
             window.alert("Você deve utilizar uma email do curso de Ciências da Computação para logar!")
         }
