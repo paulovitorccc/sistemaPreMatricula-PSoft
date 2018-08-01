3d61a2c09f1659972de38f420db2cd1c466609ea
pedrofwanderley <pedro.wanderley@ccc.ufcg.edu.br>
2018-08-01 01:03:18 -0300

    fixing

--- coordenadorController.js
+++ coordenadorController.js
@@ -1,29 +1,45 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller('coordenadorController', ['$scope', '$http', function($scope, $http) {
+myApp.controller('cooController', ['$scope', '$http', function($scope, $http) {
     
 
     $scope.cadastrarDisciplina = function(){
         var nome = document.getElementById("nome").value;
-        var msg = document.getElementById("codigo").value;
-        var author = document.getElementById("creditos").value;
-        var id = document.getElementById("carga").value;
+        var codigo = document.getElementById("codigo").value;
+        var creditos = document.getElementById("creditos").value;
+        var carga = document.getElementById("carga").value;
         var obj = {};
+        obj.semestre = null;
+        obj.pos_requisitos = [];
         obj.nome = nome;
-        obj.codigo = codigo;
         obj.creditos = creditos;
+        obj.tipo = "";
+        obj.pre_requisitos = [];
+        obj.codigo_departamento = null;
+        obj.codigo = codigo;
         obj.carga = carga;
-        enviarMsg([obj]);
+        $scope.enviarMsg([obj]);
       }
-      function enviarMsg(mensagem){
-        fetch('',{
-          method: "post" ,
-          body: JSON.stringify()
+      $scope.enviarMsg = function(info){
+        $http({
+            url: 'http://localhost:8080/prematricula/disciplinas',
+            method: "POST",
+            data: {semestre:info.semestre , pos_requisitos: info.pos_requisitos,
+                 disciplina: info.nome, 
+            creditos: info.creditos, tipo: info.tipo, pre_requisitos: info.pre_requisitos,
+             codigo_departamento: info.codigo_departamento,
+              codigo_disciplina: info.codigo,
+               horas: info.carga}
+           
         })
+        .then(function(response) {
+                window.alert('Disciplina Cadastrada');
+        }, 
+        function(response) { // optional
+                window.alert('Deu ruim');
+        });
       
       }
   
         
-
-   
 }]);
